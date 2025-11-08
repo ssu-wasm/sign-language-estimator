224e226f7e098ece8784abac2a7f00b80c5d3295
김경훈 <devkyunghoon@gmail.com>
2025-11-08 15:06:50 +0900

    Initial Commit

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,3 +1,14 @@
-int main() {
-  return 0;
+#include <emscripten/bind.h>
+
+using namespace emscripten;
+
+extern "C" {
+    int add(int a, int b) {
+        return a + b;
+    }
+}
+
+EMSCRIPTEN_BINDINGS(my_module) {
+    // Bindings can be added here in the future
+    emscripten::function("add", &add);
 }
