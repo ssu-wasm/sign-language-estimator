2a30471abbd1f62ff3de2f4c4111499241ac51be
valley <cksal0805@kakao.com>
2025-12-13 10:55:53 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: basePath,
+  assetPrefix: basePath,
   reactCompiler: true,
   turbopack: {},
   webpack: (config, { isServer }) => {
